9a4ba010e3f421805a9070fa0aa90358a651a220
ArteenM <arteenmo1@gmail.com>
2025-02-12 11:43:40 -0800

    Changed footer to look a little cleaner

--- my-website/src/app/components/Footer.tsx
+++ my-website/src/app/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-10">
+    <footer className="bg-gray-800 text-white text-center py-1 mt-10">
       <p>&copy; {new Date().getFullYear()} Arteen Mohammadi.</p>
       <div className="mt-2 space-x-4">
         <a
@@ -12,7 +12,7 @@ export default function Footer() {
           GitHub
         </a>
         <a
-          href="https://www.linkedin.com/in/arteen-mohammadi-77977631a/"
+          href="linkedin.com/in/arteen-mohammadi-77977631a/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:underline"
