f873aaaf047d1fcbb7829414f051fff82f974ed1
ArteenM <arteenmo1@gmail.com>
2025-02-16 14:41:46 -0800

    Added Some stuff for pages. Hopefully it works

--- my-website/next.config.ts
+++ my-website/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // To avoid Next.js optimizations that could break the export
+  },
+  trailingSlash: true, // Make sure URLs have trailing slashes
 };
 
-export default nextConfig;
+export default config;
